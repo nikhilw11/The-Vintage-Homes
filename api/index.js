cc30eceaf9d5682de4781f5670a257dcb8353176
Nikhil Wattamwar <nikhilwattamwar11@gmail.comclear>
2024-06-29 10:32:27 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 
 mongoose.connect("mongodb://127.0.0.1:27017/The_Vintage_Homes")
+.then (() => {
+    console.log('Connected To MongoDB');
+})
+.catch((err) => {
+    console.log(err);
+});
 
 const app = express();
 
@@ -9,3 +16,5 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000!');
     }
 );
+
+app.use("/api/user", userRouter)
