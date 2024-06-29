71a6180d8f68ad628fa2f3d44b59f69dfcecde8f
Nikhil Wattamwar <nikhilwattamwar11@gmail.comclear>
2024-06-29 11:29:33 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 mongoose.connect("mongodb://127.0.0.1:27017/The_Vintage_Homes")
 .then (() => {
@@ -12,9 +13,12 @@ mongoose.connect("mongodb://127.0.0.1:27017/The_Vintage_Homes")
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
     }
 );
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
